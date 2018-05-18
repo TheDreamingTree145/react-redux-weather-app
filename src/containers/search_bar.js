eb19f1191d148fcfb8b04ed15f48018ab82af5c9
dustin <dandersonsoftware@outlook.com>
2018-05-18 11:16:45 -0400

    setup search_bar component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCity } from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      term: ''
+    };
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      term: event.target.value
+    });
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault;
+    this.props.fetchCity(this.state.term)
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World Form</h1>
-        <form>
+        <h1>Hello World</h1>
+        <form onSubmit={this.handleOnSubmit}>
           <input
             type="text"
-          />
+            value={this.state.term}
+            onChange={this.handleOnChange}
+           />
+          <button>Get City Data</button>
         </form>
       </div>
     )
   }
 }
 
-export default SearchBar;
+export default connect(null, {fetchCity: fetchCity})(SearchBar);
